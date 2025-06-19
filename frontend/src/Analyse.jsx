de114ef8d6db253f128415db627298292d27c449
Ayushcoder1 <ayushcollege2022@gmail.com>
2025-06-19 11:09:28 +0530

    used react-window for windowing, reduces hanging/lagging

--- frontend/src/Analyse.jsx
+++ frontend/src/Analyse.jsx
@@ -1,38 +1,53 @@
-import { useAtom, useAtomValue } from "jotai";
+import { useAtomValue, useAtom } from "jotai";
+import { FixedSizeList as List } from "react-window";
 import Filter from "./components/Filter";
 import { editModeAtom, filteredAtom } from "./store/atoms";
-import TODO from "./components/TODO";
-import Card from "./components/Card";
+import TODO  from "./components/TODO";
+import Card  from "./components/Card";
 import Navbar from "./components/Navbar";
-import Edit from "./EditPage";
+import Edit  from "./EditPage";
 
-function Analyse(){
-    const [filteredTodos, ] = useAtom(filteredAtom);
-    const [editMode, setEditMode] = useAtom(editModeAtom);
+function Analyse() {
+  const filteredTodos = useAtomValue(filteredAtom);
+  const [editMode, setEditMode] = useAtom(editModeAtom);
+
+  const ITEM_HEIGHT = 150;      
+  const LIST_HEIGHT = 600; 
+
+  const Row = ({ index, style }) => {
+    const todo = filteredTodos[index];
     return (
-        <div>
-            <div>
-                <Navbar isTodos={0}/>
-            </div>
-           <div>
-            <Filter />
-           </div>
-           {filteredTodos.map(function(todo){
-            // console.log(todo);
-            return <Card  key={todo.id}>
-            <TODO todo={todo}/>
-            </Card>
-            })}
+      <div style={style}>
+        <Card key={todo.id}>
+          <TODO todo={todo} />
+        </Card>
+      </div>
+    );
+  };
 
-            {editMode && (
-                <div className='fixed inset-0 bg-white flex items-center justify-center z-50'>
-                <div className='flex justify-center full-screen shadow-3xl'>
-                    <Edit/>
-                </div>
-                </div>
-            )}
+  return (
+    <div>
+      <Navbar isTodos={0}/>
+      <Filter/>
+
+      <List
+        height={LIST_HEIGHT}
+        itemCount={filteredTodos.length}
+        itemSize={ITEM_HEIGHT}
+        width="100%"
+      >
+        {Row}
+      </List>
+
+      {editMode && (
+        <div className="fixed inset-0 bg-white flex items-center justify-center z-50">
+          <div className="flex justify-center full-screen shadow-3xl">
+            <Edit/>
+          </div>
         </div>
-    )
+      )}
+    </div>
+  );
 }
 
 export default Analyse;
--- frontend/src/Todos.jsx
+++ frontend/src/Todos.jsx
@@ -1,47 +1,60 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import TODO from './components/TODO'
 import Form from './components/Form'
 import Card from './components/Card'
 import Edit from './EditPage.jsx'
-import { todosAtom, editModeAtom, tokenAtom, get_data_Atom } from './store/atoms.jsx'
-import { useAtom, useSetAtom } from 'jotai'
+import { todosAtom, editModeAtom, get_data_Atom } from './store/atoms.jsx'
+import { useAtomValue, useAtom, useSetAtom } from 'jotai'
 import Navbar from './components/Navbar.jsx'
+import { FixedSizeList as List } from 'react-window'
 
 function Todos() {
-  const [todos , setTodos] = useAtom(todosAtom);
-  const [editMode, setEditMode] = useAtom(editModeAtom);
-  const get_data = useSetAtom(get_data_Atom);
-  const setToken = useSetAtom(tokenAtom);
+  const todos = useAtomValue(todosAtom)
+  const [editMode, setEditMode] = useAtom(editModeAtom)
+  const get_data = useSetAtom(get_data_Atom)
 
   useEffect(() => {
-    get_data();
-    return () => {
-    };
-  }, []);
+    get_data()
+  }, [])
+
+  const ITEM_HEIGHT = 150
+  const LIST_HEIGHT = 600
+
+  const Row = ({ index, style }) => {
+    const todo = todos[index]
+    return (
+      <div style={style}>
+        <Card key={todo.id}>
+          <TODO todo={todo} />
+        </Card>
+      </div>
+    )
+  }
 
   return (
-    <>
-    <div id='outer_app'>
+    <div id="outer_app">
       <Navbar isTodos={1}/>
       <Card>
         <Form />
       </Card>
-      {todos.map(function(todo){
-        // console.log(todo);
-        return <Card  key={todo.id}>
-          <TODO todo={todo}/>
-        </Card>
-      })}
+
+      <List
+        height={LIST_HEIGHT}
+        itemCount={todos.length}
+        itemSize={ITEM_HEIGHT}
+        width="100%"
+      >
+        {Row}
+      </List>
 
       {editMode && (
-        <div className='fixed inset-0 bg-white flex items-center justify-center z-50'>
-          <div className='flex justify-center full-screen shadow-3xl'>
+        <div className="fixed inset-0 bg-white flex items-center justify-center z-50">
+          <div className="flex justify-center full-screen shadow-3xl">
             <Edit/>
           </div>
         </div>
       )}
     </div>
-    </>
   )
 }
 
